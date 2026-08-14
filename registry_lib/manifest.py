4a6f6da470af29a3475823afc3e0b32387c33780
Philipp Wolfer <ph.wolfer@gmail.com>
2026-08-14 07:10:00 +0200

    ty: ignore unresolved-import for optional pygit2

--- registry_lib/manifest.py
+++ registry_lib/manifest.py
@@ -11,7 +11,7 @@ import requests
 
 
 try:
-    import pygit2
+    import pygit2  # ty: ignore[unresolved-import,unused-ignore-comment]
 except ImportError:
     pygit2 = None  # ty: ignore[invalid-assignment,unused-ignore-comment]
 
